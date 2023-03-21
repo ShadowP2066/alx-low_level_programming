2c0aeff71150924d0c0bd9c3be9192bd333969eb
ShadowP <thulilenmakhubela@gmail.com>
2023-03-21 04:42:11 -0700

    Update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/dwkwan/holbertonschool-low_level_programming/blob/master
-export LD_PRELOAD=rand.so
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
